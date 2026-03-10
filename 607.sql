e9f5b5e1826736627e2ac7d14b96a357340d7453
Donnie Adams <donnie@thedadams.com>
2026-03-10 10:14:10 -0400

    chore: add solution for problem number 607
    Signed-off-by: Donnie Adams <donnie@thedadams.com>

---
+++ 607.sql
@@ -0,0 +1,2 @@
+-- Write your PostgreSQL query statement below
+SELECT DISTINCT SalesPerson.name FROM SalesPerson WHERE SalesPerson.sales_id NOT IN (SELECT sales_id FROM Orders INNER JOIN Company ON Orders.com_id = Company.com_id WHERE Company.name = 'RED');
