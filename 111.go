85b77ec9a6ad71c0c623b811467275756580f875
Donnie Adams <donnie@thedadams.com>
2026-03-10 10:14:10 -0400

    chore: add solution for problem number 111
    Signed-off-by: Donnie Adams <donnie@thedadams.com>

---
+++ 111.go
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func minDepth(root *TreeNode) int {
+    if root == nil {
+        return 0
+    }
+
+    if root.Left == nil {
+        return 1 + minDepth(root.Right)
+    } else if root.Right == nil {
+        return 1 + minDepth(root.Left)
+    }
+
+    return 1 + min(minDepth(root.Left), minDepth(root.Right))
+}
