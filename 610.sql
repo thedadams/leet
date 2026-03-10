9daa5d5bf3b1156b6e02666354fad97f3422da15
Donnie Adams <donnie@thedadams.com>
2026-03-10 10:14:10 -0400

    chore: add solution for problem number 610
    Signed-off-by: Donnie Adams <donnie@thedadams.com>

---
+++ 610.sql
@@ -0,0 +1,7 @@
+-- Write your PostgreSQL query statement below
+SELECT x, y, z, CASE WHEN x + y <= z THEN 'No'
+                    WHEN x + z <= y THEN 'No'
+                    WHEN y + z <= x THEN 'No'
+                    ELSE 'Yes'
+                END As triangle
+FROM Triangle;
