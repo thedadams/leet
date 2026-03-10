660355656156e3cc8be1c91d75cc994fcc704597
Donnie Adams <donnie@thedadams.com>
2026-03-10 10:14:10 -0400

    chore: add solution for problem number 144
    Signed-off-by: Donnie Adams <donnie@thedadams.com>

---
+++ 144.go
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func preorderTraversal(root *TreeNode) []int {
+    if root == nil {
+        return nil
+    }
+
+    var result []int
+    result = append(result, root.Val)
+    result = append(result, preorderTraversal(root.Left)...)
+    return append(result, preorderTraversal(root.Right)...)
+}
