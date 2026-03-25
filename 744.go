1dcf606e041ced823b6d1f176829b53f68469f73
Donnie Adams <donnie@thedadams.com>
2026-03-25 07:02:34 -0400

    chore: add solution for problem number 744
    Signed-off-by: Donnie Adams <donnie@thedadams.com>

---
+++ 744.go
@@ -0,0 +1,9 @@
+func nextGreatestLetter(letters []byte, target byte) byte {
+    for _, b := range letters {
+        if b > target {
+            return b
+        }
+    }
+
+    return letters[0]
+}
