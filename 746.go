8b78c2c34bfdb30603e97b6b02f579e16ab8e201
Donnie Adams <donnie@thedadams.com>
2026-03-25 07:15:10 -0400

    chore: add solution for problem number 746
    Signed-off-by: Donnie Adams <donnie@thedadams.com>

---
+++ 746.go
@@ -0,0 +1,8 @@
+func minCostClimbingStairs(cost []int) int {
+    dp := make([]int, len(cost) + 2)
+    for i := len(cost) - 1; i >= 0; i-- {
+        dp[i] = cost[i] + min(dp[i+1], dp[i+2])
+    }
+
+    return min(dp[0], dp[1])
+}
